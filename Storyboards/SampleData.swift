a6c9e36953d59d5303302813d73346c5939894a9
Ronald Beasley <rgbjr123@gmail.com>
2015-08-13 12:05:16 -0500

    1st change

---
+++ Storyboards/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  Storyboards
+//
+//  Created by Ronald Beasley on 8/3/15.
+//  Copyright (c) 2015 IBM. All rights reserved.
+//
+
+import Foundation
+
+//Set up sample data
+
+let playersData = [ Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
